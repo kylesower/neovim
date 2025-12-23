7c9de9e4635fcf43df04169940007e954112d4f2
Kyle Sower <gitlab@leastaction.me>
2025-12-22 22:36:51 -0600

    Try two separate sequences

--- runtime/lua/vim/_defaults.lua
+++ runtime/lua/vim/_defaults.lua
@@ -875,7 +875,8 @@ do
 
       -- Send OSC 11 query along with DA1 request to determine whether terminal
       -- supports the query. #32109
-      vim.api.nvim_ui_send('\027]11;?\007\027[c')
+      vim.api.nvim_ui_send('\027]11;?\007')
+      vim.api.nvim_ui_send('\027[c')
 
       -- Wait until detection of OSC 11 capabilities is complete to
       -- ensure background is automatically set before user config.
